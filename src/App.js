e27631c0f70ffd257ab0d526b52242025c4b59f8
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-07-15 10:44:36 -0300

    wrapping Transition components

--- src/App.js
+++ src/App.js
@@ -52,11 +52,7 @@ class App extends Component {
             />
           )}
         </Transition>
-        <Transition in={this.state.modalIsOpen} timeout={300}>
-          {(state) => (
-            <Modal show={state} closed={this.closeModal} />
-          )}
-        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModal}>
           Open Modal
